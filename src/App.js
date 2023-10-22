ee516b77754f4a0eba3537e370f84cd792f05d40
Chaerin Park <rinapark19@gmail.com>
2023-10-22 23:23:36 +0900

    add background

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TopCloset from './TopCloset'
 import {BrowserView, MobileView} from 'react-device-detect';
 import React, {useState} from 'react';
 import bodyImage from './img/body.png';
+import backgroundImage from './img/bg.png';
 
 function App() {
   const [currentImage, setCurrentImage] = useState(bodyImage);
@@ -15,6 +16,7 @@ function App() {
   return (
     <div className="App">
       <BrowserView>
+        <img src={backgroundImage} className='bg'/>
         <TopCloset changeImage={changeBodyImage}/>
         <Body bodyImage={currentImage}/>
       </BrowserView>
